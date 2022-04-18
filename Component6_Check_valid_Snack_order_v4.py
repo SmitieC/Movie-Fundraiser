8d4d27ea9db0f4fe2c875845ba353aa8677360a7
SmitieC <83625219+SmitieC@users.noreply.github.com>
2022-04-18 18:09:23 +1200

    component 6

--- Component6_Check_valid_Snack_order_v4.py
+++ Component6_Check_valid_Snack_order_v4.py
@@ -1,5 +1,3 @@
-"""Incorporate component 5 and 6 into one script"""
-
 def get_choice(question, valid_choices):
     choice_error = "Sorry, that is not a valid choice"
     choice = input(question).lower()
@@ -12,24 +10,29 @@ def get_choice(question, valid_choices):
     return get_choice(question, valid_choices)
 
 
-ask_for_snack = "What snack do you want - 'x' to stop ordering: "
+ask_for_snacks = "What snack do you want - 'x' to stop ordering: "
 valid_snacks = [["popcorn", "p", "corn", "1"], ["m&ms", "mms", "m", "2"],
-                    ["pita chips", "chips", "pc", "pita", "c", "3"],
-                    ["water", "w", "4"], ["x", "X", "exit", "5"]]
-
+                ["pita chips", "chips", "pc", "pita", "c", "3"],
+                ["water", "w", "4"], ["x", "X", "exit", "5"]]
 check_snacks = "Do you want snacks? "
 valid_yes_no = [["y", "yes"], ["n", "no"]]
+snacks_required = get_choice(check_snacks, valid_yes_no)
+snack_order = []
 
 getting_snacks = True
-snacks_required = get_choice(check_snacks, valid_yes_no)
 while getting_snacks:
     if snacks_required == "N":
-        print("You don't want snacks")
         getting_snacks = False
     else:
-        option = get_choice(ask_for_snack, valid_snacks)
+        option = get_choice(ask_for_snacks, valid_snacks)
         if option != "X":
-            print(f"You have chosen {option}")
+            snack_order.append(option)
         else:
             getting_snacks = False
-            print("Thanks for ordering your snacks")
+
+if len(snack_order) > 0:
+    print("\nThis is a summary of your order:")
+    for item in snack_order:
+        print(f"\t{item}")
+else:
+    print("No snacks were ordered.")
